68ed7e6b2d57b454ec293eb24cae92212068d98b
Minato3000 <101650634+Minato3000@users.noreply.github.com>
2023-01-06 19:49:29 +0530

    first commit

--- translator_gui.py
+++ translator_gui.py
@@ -4,5 +4,10 @@ from tkinter import ttk
 window = Tk()
 
 
+textfield = Label(window, text='Chat')
+textfield.pack()
+
+next_button = Button(window, text='Next')
+next_button.pack()
 
 window.mainloop()
