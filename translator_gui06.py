89cd08c1b7c177c98e4e43360633702e0f31f0da
Minato3000 <101650634+Minato3000@users.noreply.github.com>
2023-01-12 10:29:29 +0530

    version 06 translator gui The final version in tkinter

--- translator_gui06.py
+++ translator_gui06.py
@@ -1,17 +1,17 @@
 import os  # OS to do operations on system
 from tkinter import Tk, END, Frame, Text, Button, Label, CENTER, BOTH, TOP, BOTTOM, LEFT  # Tkinter necessary functions
-from tkinter.font import Font # Tkinter's font function to customize font
-from tkinter.ttk import Combobox # Tkinter's Combobox for language selection
-from textblob import TextBlob # Textblob to store and manipulate text
+from tkinter.font import Font  # Tkinter's font function to customize font
+from tkinter.ttk import Combobox  # Tkinter's Combobox for language selection
+from textblob import TextBlob  # Textblob to store and manipulate text
 
 from googletrans import Translator, LANGUAGES  # To translate the text using google's translator
 # googletrans version 3.1.0a0c
 
-from gtts import gTTS # To convert text to speech
+from gtts import gTTS  # To convert text to speech
 from playsound import playsound  # playsound version 1.2.2
 
-import pyttsx3 as pyt # For accessing text to speech voices
-import speech_recognition as sr # To recognize the speech
+import pyttsx3 as pyt  # For accessing text to speech voices
+import speech_recognition as sr  # To recognize the speech
 
 
 class App(Tk):
@@ -33,6 +33,15 @@ class App(Tk):
         # Place the translator frame in grid
         self.right = TranslateFrame(self)
         self.right.grid(row=1, column=1)
+
+
+# Lang_code function
+# To convert the selected language name to the language code
+def lang_code(self, lang_option):
+    for key, value in self.languages.items():
+        if value == lang_option.get():
+            code = key
+            return code
 
 
 # Refresh function
@@ -62,6 +71,11 @@ class originalFrame(Frame):
         self.original_text = Text(self, bg="azure2", fg="black", width=50, height=20, font=basic_font, padx=4, pady=8)
         self.original_text.pack(side=TOP, fill=BOTH, expand=True)
 
+        # Speaks the content - Button widget
+        self.speak_btn = Button(self, text="Speak", padx=8, pady=3, font=basic_font, bg='#45b592', fg='#ffffff',
+                                command=self.speak)
+        self.speak_btn.pack(side=LEFT, fill=BOTH, expand=True)
+
         # The languages in the googletrans
         self.languages = LANGUAGES
         language_list = list(self.languages.values())
@@ -76,8 +90,37 @@ class originalFrame(Frame):
                                     command=self.translate)
         self.translate_btn.pack(side=LEFT, fill=BOTH, expand=True)
 
-    # Translation function
-    # Translate the text in this frame to the next frame
+    # The capture function
+    # Records the audio and recognizes the speech text
+    def capture(self):
+        rec = sr.Recognizer()
+        mc = sr.Microphone()
+        lang = lang_code(self, self.from_language_option)
+        with mc as source:
+            print("Speak Now !")
+            rec.adjust_for_ambient_noise(source, duration=0.2)
+            try:
+                audio = rec.listen(source)
+                MyText = rec.recognize_google(audio, language=lang)
+                refresh(self.original_text)
+                self.original_text.insert(1.0, MyText)
+            except sr.UnknownValueError:
+                print("Unable to Understand the Input")
+            except sr.RequestError:
+                print("Unable to provide Required Output")
+
+    # The speak function
+    # Produces audio of the text obtained
+    def speak(self):
+        text = self.original_text.get(1.0, END)
+        lang = lang_code(self, self.from_language_option)
+        tts = gTTS(text, lang=lang, slow=False)
+        tts.save('./audio/original_audio_msg.mp3')
+        playsound('./audio/original_audio_msg.mp3')
+        os.remove('./audio/original_audio_msg.mp3')
+
+    # The translate function
+    # Convert the text of a language to another language
     def translate(self):
         # Textblob used to store and manipulate string
         text = TextBlob(self.original_text.get(1.0, END))
@@ -86,11 +129,11 @@ class originalFrame(Frame):
         translator = Translator()
 
         # Obtain the language code
-        self.from_lang = self.getlangcode()
-        self.to_lang = self.parent.right.getlangcode()
+        from_lang = lang_code(self, self.from_language_option)
+        to_lang = lang_code(self, self.parent.right.to_language_option)
 
         # Translate the text
-        context = translator.translate(text, src=self.from_lang, dest=self.to_lang)
+        context = translator.translate(text, src=from_lang, dest=to_lang)
         refresh(self.parent.right.translated_text)
 
         self.parent.right.translated_text.configure(state='normal')
@@ -98,17 +141,6 @@ class originalFrame(Frame):
         self.parent.right.translated_text.configure(state='disabled')
 
         refresh(self.original_text)
-
-    # Obtain language code function
-    def getlangcode(self):
-        for key, value in self.languages.items():
-            if value == self.from_language_option.get():
-                langcode = key
-                return langcode
-
-    # Record audio function
-    def capture(self):
-        pass
 
 
 # The translation frame
@@ -140,6 +172,11 @@ class TranslateFrame(Frame):
         self.languages = LANGUAGES
         language_list = list(self.languages.values())
 
+        # To translate the text - Translate Button
+        self.translate_btn = Button(self, text="Translate", padx=8, pady=3, font=basic_font, bg='#45b592', fg='#ffffff',
+                                    command=self.translate)
+        self.translate_btn.pack(side=LEFT, fill=BOTH, expand=True)
+
         # Selecting the language - Combobox
         self.to_language_option = Combobox(self, width=20, values=language_list, font=basic_font)
         self.to_language_option.current(93)
@@ -150,47 +187,60 @@ class TranslateFrame(Frame):
                                 command=self.speak)
         self.speak_btn.pack(side=BOTTOM, fill=BOTH, expand=True)
 
-    # Obtain language code function
-    def getlangcode(self):
-        for key, value in self.languages.items():
-            if value == self.to_language_option.get():
-                langcode = key
-                return langcode
-
-    # Speak function
-    def speak(self):
-        text = self.translated_text.get(1.0, END)
-        lang = self.parent.left.to_lang
-        tts = gTTS(text, lang=lang, slow=False)
-        tts.save('./audio/audio_msg.mp3')
-        playsound('./audio/audio_msg.mp3')
-        os.remove('./audio/audio_msg.mp3')
-
-    # Capture function
+    # The capture function
+    # Records the audio and recognizes the speech text
     def capture(self):
         rec = sr.Recognizer()
         mc = sr.Microphone()
-        translator = Translator()
-        from_lang = 'en'
-        to_lang = 'ta'
+        lang = lang_code(self, self.to_language_option)
         with mc as source:
             print("Speak Now !")
             rec.adjust_for_ambient_noise(source, duration=0.2)
             try:
                 audio = rec.listen(source)
-                MyText = rec.recognize_google(audio)
-                trans_text = translator.translate(MyText, src=from_lang, dest=to_lang)
-                self.translated_text.insert(1.0, trans_text.text)
+                MyText = rec.recognize_google(audio, language=lang)
+                refresh(self.translated_text)
+                self.translated_text.insert(1.0, MyText)
             except sr.UnknownValueError:
                 print("Unable to Understand the Input")
             except sr.RequestError:
                 print("Unable to provide Required Output")
 
-    def record_speaker(self, text):
-        self.engine.say(text)
-        self.engine.runAndWait()
+    # The speak function
+    # Produces audio of the text obtained
+    def speak(self):
+        text = self.translated_text.get(1.0, END)
+        lang = lang_code(self, self.to_language_option)
+        tts = gTTS(text, lang=lang, slow=False)
+        tts.save('./audio/translated_audio_msg.mp3')
+        playsound('./audio/translated_audio_msg.mp3')
+        os.remove('./audio/translated_audio_msg.mp3')
+
+    # The translate function
+    # Convert the text of a language to another language
+    def translate(self):
+        # Textblob used to store and manipulate string
+        text = TextBlob(self.translated_text.get(1.0, END))
+
+        # To translate using google trans
+        translator = Translator()
+
+        # Obtain the language code
+        from_lang = lang_code(self, self.to_language_option)
+        to_lang = lang_code(self, self.parent.left.from_language_option)
+
+        # Translate the text
+        context = translator.translate(text, src=from_lang, dest=to_lang)
+        refresh(self.parent.left.original_text)
+
+        self.parent.left.original_text.configure(state='normal')
+        self.parent.left.original_text.insert(1.0, context.text)
+        self.parent.left.original_text.configure(state='disabled')
+
+        refresh(self.translated_text)
 
 
+# The heading frame
 class HeadingFrame(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)
@@ -209,6 +259,7 @@ class HeadingFrame(Frame):
         subheading.place(relx=0.5, rely=0.7, anchor=CENTER)
 
 
+# The name function runs the whole translator app
 if __name__ == "__main__":
     translator = App()
     translator.mainloop()
